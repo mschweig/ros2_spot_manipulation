8d6e4cdd986f1caaebfc4f7714680a2a0720b707
Timon NX <noemail@timon.com>
2025-04-18 09:31:28 +0000

    add basic grasp logic
    Signed-off-by: Timon NX <noemail@timon.com>

--- spot_manipulation_interface/src/grasp_logic.py
+++ spot_manipulation_interface/src/grasp_logic.py
@@ -1,74 +1,190 @@
-def arm_object_grasp(config, pixel_x, pixel_y):
-    import bosdyn.client.util
-    from bosdyn.api import geometry_pb2, manipulation_api_pb2, image_pb2
-    from bosdyn.client.frame_helpers import VISION_FRAME_NAME
-    from bosdyn.client.image import ImageClient
-    from bosdyn.client.manipulation_api_client import ManipulationApiClient
-    from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
-    from bosdyn.client.robot_state import RobotStateClient
-    from bosdyn.client.lease import LeaseKeepAlive
-    from bosdyn.client.estop import EstopClient
-    from bosdyn.client.frame_helpers import get_vision_tform_body, math_helpers
-    import numpy as np
-    import cv2
-    import time
+# Copyright (c) 2023 Boston Dynamics, Inc.  All rights reserved.
+#
+# Downloading, reproducing, distributing or otherwise using the SDK Software
+# is subject to the terms and conditions of the Boston Dynamics Software
+# Development Kit License (20191101-BDSDK-SL).
 
-    sdk = bosdyn.client.create_standard_sdk('ArmObjectGraspClient')
-    robot = sdk.create_robot(config.hostname)
-    bosdyn.client.util.authenticate(robot)
+"""Tutorial to show how to use Spot's arm.
+"""
+import time
+
+import bosdyn.client
+import bosdyn.client.estop
+import bosdyn.client.lease
+import bosdyn.client.util
+from bosdyn.api import estop_pb2, geometry_pb2, image_pb2, manipulation_api_pb2
+from bosdyn.client.estop import EstopClient
+from bosdyn.client.frame_helpers import VISION_FRAME_NAME, get_vision_tform_body, math_helpers
+from bosdyn.client.image import ImageClient
+from bosdyn.client.manipulation_api_client import ManipulationApiClient
+from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
+from bosdyn.client.robot_state import RobotStateClient
+
+
+def add_grasp_constraint(config, grasp, robot_state_client):
+    # There are 3 types of constraints:
+    #   1. Vector alignment
+    #   2. Full rotation
+    #   3. Squeeze grasp
+    #
+    # You can specify more than one if you want and they will be OR'ed together.
+
+    # For these options, we'll use a vector alignment constraint.
+    use_vector_constraint = config.force_top_down_grasp or config.force_horizontal_grasp
+
+    # Specify the frame we're using.
+    grasp.grasp_params.grasp_params_frame_name = VISION_FRAME_NAME
+
+    if use_vector_constraint:
+        if config.force_top_down_grasp:
+            # Add a constraint that requests that the x-axis of the gripper is pointing in the
+            # negative-z direction in the vision frame.
+
+            # The axis on the gripper is the x-axis.
+            axis_on_gripper_ewrt_gripper = geometry_pb2.Vec3(x=1, y=0, z=0)
+
+            # The axis in the vision frame is the negative z-axis
+            axis_to_align_with_ewrt_vo = geometry_pb2.Vec3(x=0, y=0, z=-1)
+
+        if config.force_horizontal_grasp:
+            # Add a constraint that requests that the y-axis of the gripper is pointing in the
+            # positive-z direction in the vision frame.  That means that the gripper is constrained to be rolled 90 degrees and pointed at the horizon.
+
+            # The axis on the gripper is the y-axis.
+            axis_on_gripper_ewrt_gripper = geometry_pb2.Vec3(x=0, y=1, z=0)
+
+            # The axis in the vision frame is the positive z-axis
+            axis_to_align_with_ewrt_vo = geometry_pb2.Vec3(x=0, y=0, z=1)
+
+        # Add the vector constraint to our proto.
+        constraint = grasp.grasp_params.allowable_orientation.add()
+        constraint.vector_alignment_with_tolerance.axis_on_gripper_ewrt_gripper.CopyFrom(
+            axis_on_gripper_ewrt_gripper)
+        constraint.vector_alignment_with_tolerance.axis_to_align_with_ewrt_frame.CopyFrom(
+            axis_to_align_with_ewrt_vo)
+
+        # We'll take anything within about 10 degrees for top-down or horizontal grasps.
+        constraint.vector_alignment_with_tolerance.threshold_radians = 0.17
+
+    elif config.force_45_angle_grasp:
+        # Demonstration of a RotationWithTolerance constraint.  This constraint allows you to
+        # specify a full orientation you want the hand to be in, along with a threshold.
+        #
+        # You might want this feature when grasping an object with known geometry and you want to
+        # make sure you grasp a specific part of it.
+        #
+        # Here, since we don't have anything in particular we want to grasp,  we'll specify an
+        # orientation that will have the hand aligned with robot and rotated down 45 degrees as an
+        # example.
+
+        # First, get the robot's position in the world.
+        robot_state = robot_state_client.get_robot_state()
+        vision_T_body = get_vision_tform_body(robot_state.kinematic_state.transforms_snapshot)
+
+        # Rotation from the body to our desired grasp.
+        body_Q_grasp = math_helpers.Quat.from_pitch(0.785398)  # 45 degrees
+        vision_Q_grasp = vision_T_body.rotation * body_Q_grasp
+
+        # Turn into a proto
+        constraint = grasp.grasp_params.allowable_orientation.add()
+        constraint.rotation_with_tolerance.rotation_ewrt_frame.CopyFrom(vision_Q_grasp.to_proto())
+
+        # We'll accept anything within +/- 10 degrees
+        constraint.rotation_with_tolerance.threshold_radians = 0.17
+
+    elif config.force_squeeze_grasp:
+        # Tell the robot to just squeeze on the ground at the given point.
+        constraint = grasp.grasp_params.allowable_orientation.add()
+        constraint.squeeze_grasp.SetInParent()
+
+def arm_object_grasp():
+    """A simple example of using the Boston Dynamics API to command Spot's arm."""
+
+    sdk = bosdyn.client.create_standard_sdk('ROS2 Grasp Action')
+    robot = sdk.create_robot('192.168.50.3')
+    robot.authenticate('user', 'vfx43te7ocvq')
     robot.time_sync.wait_for_sync()
 
     assert robot.has_arm(), 'Robot requires an arm to run this example.'
-    client = robot.ensure_client(EstopClient.default_service_name)
-    if client.get_status().stop_level != 0:
-        raise Exception('Robot is estopped')
 
-    lease_client = robot.ensure_client('lease')
-    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+    lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
     image_client = robot.ensure_client(ImageClient.default_service_name)
     manipulation_api_client = robot.ensure_client(ManipulationApiClient.default_service_name)
 
-    with LeaseKeepAlive(lease_client):
+    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+        # Now, we are ready to power on the robot. This call will block until the power
+        # is on. Commands would fail if this did not happen. We can also check that the robot is
+        # powered at any point.
+        robot.logger.info('Powering on robot... This may take a several seconds.')
         robot.power_on(timeout_sec=20)
-        blocking_stand(robot.ensure_client(RobotCommandClient.default_service_name))
+        assert robot.is_powered_on(), 'Robot power on failed.'
+        robot.logger.info('Robot powered on.')
 
-        image = image_client.get_image_from_sources([config.image_source])[0]
-        pick_vec = geometry_pb2.Vec2(x=pixel_x, y=pixel_y)
+        # Tell the robot to stand up. The command service is used to issue commands to a robot.
+        # The set of valid commands for a robot depends on hardware configuration. See
+        # RobotCommandBuilder for more detailed examples on command building. The robot
+        # command service requires timesync between the robot and the client.
+        robot.logger.info('Commanding robot to stand...')
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        blocking_stand(command_client, timeout_sec=10)
+        robot.logger.info('Robot standing.')
 
+        # Take a picture with a camera
+        robot.logger.info('Getting an image from: %s', 'hand_image')
+        image_responses = image_client.get_image_from_sources(['hand_image'])
+
+        if len(image_responses) != 1:
+            print(f'Got invalid number of images: {len(image_responses)}')
+            print(image_responses)
+            assert False
+
+        pick_vec = geometry_pb2.Vec2(x=50, y=60)
+
+        # Build the proto
+        image = image_responses[0]
         grasp = manipulation_api_pb2.PickObjectInImage(
-            pixel_xy=pick_vec,
-            transforms_snapshot_for_camera=image.shot.transforms_snapshot,
+            pixel_xy=pick_vec, transforms_snapshot_for_camera=image.shot.transforms_snapshot,
             frame_name_image_sensor=image.shot.frame_name_image_sensor,
             camera_model=image.source.pinhole)
 
-        # Grasp constraints
-        if config.force_top_down_grasp:
-            axis_on_gripper = geometry_pb2.Vec3(x=1, y=0, z=0)
-            axis_align_with = geometry_pb2.Vec3(x=0, y=0, z=-1)
-        elif config.force_horizontal_grasp:
-            axis_on_gripper = geometry_pb2.Vec3(x=0, y=1, z=0)
-            axis_align_with = geometry_pb2.Vec3(x=0, y=0, z=1)
-        else:
-            axis_on_gripper = axis_align_with = None
-
-        if axis_on_gripper:
-            grasp.grasp_params.grasp_params_frame_name = VISION_FRAME_NAME
-            constraint = grasp.grasp_params.allowable_orientation.add()
-            constraint.vector_alignment_with_tolerance.axis_on_gripper_ewrt_gripper.CopyFrom(axis_on_gripper)
-            constraint.vector_alignment_with_tolerance.axis_to_align_with_ewrt_frame.CopyFrom(axis_align_with)
-            constraint.vector_alignment_with_tolerance.threshold_radians = 0.17
-
+        # Ask the robot to pick up the object
         grasp_request = manipulation_api_pb2.ManipulationApiRequest(pick_object_in_image=grasp)
-        cmd_response = manipulation_api_client.manipulation_api_command(grasp_request)
 
+        # Send the request
+        cmd_response = manipulation_api_client.manipulation_api_command(
+            manipulation_api_request=grasp_request)
+
+        # Get feedback from the robot
+        return grasp_request
         while True:
-            feedback = manipulation_api_client.manipulation_api_feedback_command(
-                manipulation_api_pb2.ManipulationApiFeedbackRequest(
-                    manipulation_cmd_id=cmd_response.manipulation_cmd_id))
-            state = feedback.current_state
-            if state in [3, 4]:  # succeeded or failed
+            feedback_request = manipulation_api_pb2.ManipulationApiFeedbackRequest(
+                manipulation_cmd_id=cmd_response.manipulation_cmd_id)
+
+            # Send the request
+            response = manipulation_api_client.manipulation_api_feedback_command(
+                manipulation_api_feedback_request=feedback_request)
+
+            print(
+                f'Current state: {manipulation_api_pb2.ManipulationFeedbackState.Name(response.current_state)}'
+            )
+
+            if response.current_state == manipulation_api_pb2.MANIP_STATE_GRASP_SUCCEEDED or response.current_state == manipulation_api_pb2.MANIP_STATE_GRASP_FAILED:
                 break
+
             time.sleep(0.25)
 
-        robot.power_off(cut_immediately=False)
-        return state == 3
+        robot.logger.info('Finished grasp.')
+        time.sleep(4.0)
+
+        robot.logger.info('Sitting down and turning off.')
+
+        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
+        # is issued to the robot. This will attempt to sit the robot before powering off.
+        robot.power_off(cut_immediately=False, timeout_sec=20)
+        assert not robot.is_powered_on(), 'Robot power off failed.'
+        robot.logger.info('Robot safely powered off.')
+
+
+
+
+
--- spot_manipulation_interface/src/spot_grasp_action_server.py
+++ spot_manipulation_interface/src/spot_grasp_action_server.py
@@ -1,42 +1,33 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
-from your_package.action import GraspObject
-from your_package.grasp_logic import arm_object_grasp
-from threading import Thread
+from spot_manipulation_interface.action import Grasp
+from grasp_logic import arm_object_grasp
+
 
 class SpotGraspActionServer(Node):
     def __init__(self):
         super().__init__('spot_grasp_action_server')
-        self._action_server = ActionServer(self, GraspObject, 'grasp_object', self.execute_callback)
+        self._action_server = ActionServer(self, Grasp, 'grasp', self.execute_callback)
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Grasp action received')
+        feedback_msg = Grasp.Feedback()
+        feedback_msg.current_state = 'Starting Grasp'
+        result = Grasp.Result()
+        result.message = arm_object_grasp()
+        goal_handle.succeed()  
+        return result
 
-        class Config:
-            hostname = goal_handle.request.hostname
-            image_source = goal_handle.request.image_source
-            force_top_down_grasp = goal_handle.request.force_top_down_grasp
-            force_horizontal_grasp = goal_handle.request.force_horizontal_grasp
-            force_45_angle_grasp = goal_handle.request.force_45_angle_grasp
-            force_squeeze_grasp = goal_handle.request.force_squeeze_grasp
-            verbose = True
+def main(args=None):
+    rclpy.init(args=args)
 
-        def run():
-            try:
-                success = arm_object_grasp(Config(), goal_handle.request.pixel_x, goal_handle.request.pixel_y)
-                goal_handle.succeed()
-                result = GraspObject.Result()
-                result.success = success
-                result.message = 'Grasp success' if success else 'Grasp failed'
-                return result
-            except Exception as e:
-                goal_handle.abort()
-                result = GraspObject.Result()
-                result.success = False
-                result.message = str(e)
-                return result
+    spot_grasp_action_server = SpotGraspActionServer()
 
-        thread = Thread(target=run)
-        thread.start()
-        return rclpy.task.Future()
+    rclpy.spin(spot_grasp_action_server)
+
+
+if __name__ == '__main__':
+    main()
+
+       
